910906b398b9a23cc5b7d81ceced7bd700d1d131
coder <79178809+c0deblack@users.noreply.github.com>
2023-03-22 15:42:22 -0400

    Added some comments

--- backend/src/main/java/org/barp/backend/Model/MoneyAccount.java
+++ backend/src/main/java/org/barp/backend/Model/MoneyAccount.java
@@ -14,6 +14,14 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MoneyAccount {
+    /**
+     * This is used to create a list of predefined values that can be used
+     * in the account type column.
+     *
+     * The corresponding field is annotated like this:
+     *      @Enumerated(EnumType.STRING)
+     *      public AccountType accountType;
+     */
     public enum AccountType {
         Checking,
         Savings
@@ -30,6 +38,21 @@ public class MoneyAccount {
     @JsonBackReference
     public User user;
 
+    /**
+     * @OneToMany
+     * This relationship creates a parent (MoneyAccount) and child (Transaction) relationship.
+     *
+     * The mappedBy tells Spring which field in the Transctions table points to a MoneyAccount.
+     *
+     * Cascade defines how records with foreign keys should be updated. Currently, the relationship
+     * is configured as follows:
+     *
+     * CascadeType.REMOVE
+     * Deleting a MoneyAccount will delete all associated transactions.
+     *
+     * CascadeType.DETACH
+     * Deleting a transaction removes the reference to it in the MoneyAccount table.
+     */
     @OneToMany(mappedBy = "moneyAccount", cascade = {CascadeType.DETACH, CascadeType.REMOVE}, fetch = FetchType.EAGER)
     @JsonManagedReference
     public List<Transaction> transactions;
